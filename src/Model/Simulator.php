9265f8723fa1cbcb83327c1d92fa4fd90bbef6ba
gpdrsilva <gpdrsilva@uolinc.com>
2019-10-18 11:30:49 -0300

    Maior expressividade

--- src/Model/Simulator.php
+++ src/Model/Simulator.php
@@ -10,18 +10,18 @@ final class Simulator
     /**
      * Simula o calculo de frete retornando o valor monetário a ser cobrado
      *
-     * @param Money $costKiloPerKilometer
-     * @param Weight $weight
+     * @param Money    $cost
+     * @param Weight   $weight
      * @param Distance $distance
      * @return Money
      */
-    public function simulate(Money $costKiloPerKilometer, Weight $weight, Distance $distance): Money
+    public function simulate(Money $cost, Weight $weight, Distance $distance): Money
     {
         $kilos = $weight->toKilo();
         $kilometers = $distance->toKilometer();
 
-        $value = $costKiloPerKilometer->getValue()->getValue() * $kilos->getValue()->getValue() * $kilometers->getValue()->getValue();
+        $value = $cost->getValue()->getValue() * $kilos->getValue()->getValue() * $kilometers->getValue()->getValue();
 
-        return new Money($costKiloPerKilometer->getCurrency(), new PositiveDecimal($value));
+        return new Money($cost->getCurrency(), new PositiveDecimal($value));
     }
 }
